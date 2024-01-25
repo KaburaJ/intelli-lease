b0121c993a709f92f07d0614251c6bd8e529e806
Joan Kabura <kaburajoan25@gmail.com>
2024-01-26 02:26:45 +0300

    feat:solving leasing errors

--- backend/LeasingLogic/src/routers/LogicRoutes.js
+++ backend/LeasingLogic/src/routers/LogicRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const logicRoutes = express.Router();
-const addLandLeasingDetails =require('../controllers/addLandLeasingDetails')
+const addLandLeasingDetails =require('../controllers/addLandLeasingDetails');
+const getUserDetails = require('../controllers/getUserDetails');
 
 
 logicRoutes.post('/lease-out', addLandLeasingDetails)
+logicRoutes.post('/userdetails', getUserDetails)
 
 
 module.exports = logicRoutes;
