862d72b3dae208432108fe9a6ddfbec5b8557e0c
Joan Kabura <kaburajoan25@gmail.com>
2024-01-26 23:10:09 +0300

    feat: added jwts1

--- backend/Auth/src/middlewares/authMiddleware.js
+++ backend/Auth/src/middlewares/authMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
   const token = req.headers['authorization'];
+  console.log('Received Token:', token);
   if (!token) return res.status(401).json({ message: 'No token provided' });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
